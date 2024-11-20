18c816eeb5386048414274913969dba05e0ce6d4
DEDSEC-WEAPON\minh <mnguyenngoc69@gmail.com>
2024-11-20 17:37:48 +0700

    commit testing

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const systemConfig = require("./config/system")
+const bodyParser = require('body-parser');
+const flash = require('express-flash')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const path = require('path');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+const database = require("./config/database");
+database.connect();
+
+const routeAdmin = require("./routes/admin/index.route")
+const routeClient = require("./routes/client/index.route");
+
+app.set('views', `${__dirname}/views`) //tìm đến thư mục tên là views
+app.set('view engine', 'pug') // xác định view engine có đuôi .pug
+
+app.use(express.static(`${__dirname}/public`)) // thiết lập thư mục chứa file tĩnh như js images css
+
+//Khai báo biến toàn cục cho file pug
+app.locals.prefixAdmin = systemConfig.prefixAdmin;//file: system.js - folder: config
+
+//lib bodyparser dưới dạng form
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//lib bodyparser
+app.use(bodyParser.json())
+
+//lib flash
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+
+//override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
+// New Route to the TinyMCE Node module 
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+
+//Khai báo đường dẫn
+routeAdmin.index(app);
+routeClient.index(app);   
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ routes/client/index.route.js
@@ -0,0 +1,29 @@
+const homeRoute = require("./home.route")
+const productRoute = require("./product.route")
+const cartRoute = require("./cart.route")
+const orderRoute = require("./order.route")
+const userRoute = require("./user.route")
+
+const categoryMiddleware = require("../../middlewares/client/category.middleware")
+const cartMiddleware = require("../../middlewares/client/cart.middleware")
+const userMiddleware = require("../../middlewares/client/user.middleware")
+const settingMiddleware = require("../../middlewares/client/setting.middleware")
+
+module.exports.index = (app) => {
+    app.use(categoryMiddleware.category)
+    app.use(cartMiddleware.cart)
+    app.use(userMiddleware.infoUser)
+    app.use(settingMiddleware.general)
+    
+    app.use('/products', productRoute)
+    app.use('/cart', cartRoute)
+    app.use('/order', orderRoute)
+    app.use('/user', userRoute)
+    app.use('/', homeRoute)
+    
+    app.get('*', (req, res) => {
+      res.render("client/pages/errors/404", {
+        pageTitle: "404 Not Found",
+      });
+    });
+}
